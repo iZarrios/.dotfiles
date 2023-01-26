0acf627249a35e4fd919a511d0c7e4382153e5f9
iZarrios <zarriose@gmail.com>
2023-01-26 17:02:27 +0200

    duplicate parser and treesitter

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,41 +1,10 @@
 local lsp = require("lsp-zero")
-,
-})
-
--- Fix Undefined global 'vim'
-lsp.configure('sumneko_lua', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-})
-
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-})
-
--- TODO:
--- disable completion with tab
--- this helps with copilot setup
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
 
 lsp.preset("recommended")
 
 lsp.ensure_installed({
     'tsserver',
     'sumneko_lua',
-    'clangd',
-    -- 'rust_analyzer',
 })
 
 -- Fix Undefined global 'vim'
