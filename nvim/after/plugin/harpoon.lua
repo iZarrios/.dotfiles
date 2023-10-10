6307b39c52a00b5f4ecf747b3b18bea1c1e43969
iZarrios <zarriose@gmail.com>
2023-10-10 16:15:26 +0300

    add: harpoon bindings

---
+++ nvim/after/plugin/harpoon.lua
@@ -0,0 +1,11 @@
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+-- I think mapping more than 2 files is a bit extreme for now?
+vim.keymap.set("n", "<C-q>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-w>", function() ui.nav_file(2) end)
+
