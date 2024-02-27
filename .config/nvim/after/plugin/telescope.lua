809a5052bf100c7694138955b2d0dd6d1bc6c0cf
iZarrios <zarriose@gmail.com>
2024-02-27 04:08:13 +0200

    update: add opts

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,11 +1,15 @@
 local builtin = require('telescope.builtin')
 
+local opts = {
+    noremap = true,
+    silent = true
+};
 
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, opts)
+vim.keymap.set('n', '<leader>ff', builtin.find_files, opts)
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, opts)
+vim.keymap.set('n', '<leader>fb', builtin.buffers, opts)
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, opts)
 vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({search = vim.fn.input("Grep > ")});
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
