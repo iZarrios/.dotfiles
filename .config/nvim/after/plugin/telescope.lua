a122c5728fcfd661d27227ef6b1f9d0099b89753
iZarrios <zarriose@gmail.com>
2024-04-09 13:49:06 +0200

    add: diagnostics search

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,15 +1,21 @@
 local builtin = require('telescope.builtin')
 
+pcall(require('telescope').load_extension, 'fzf')
+
 local opts = {
     noremap = true,
     silent = true
 };
 
+
 vim.keymap.set('n', '<C-p>', builtin.git_files, opts)
 vim.keymap.set('n', '<leader>ff', builtin.find_files, opts)
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, opts)
-vim.keymap.set('n', '<leader>fb', builtin.buffers, opts)
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, opts)
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set('n', '<leader>sd', builtin.diagnostics, opts)
+
+-- I never use this
+-- vim.keymap.set('n', '<leader>fb', builtin.buffers, opts)
