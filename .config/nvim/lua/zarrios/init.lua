d03b4f5d2f400acaee2ccb8c70a88f8760515554
iZarrios <zarriose@gmail.com>
2024-04-09 13:48:44 +0200

    remove: unecessary global options change; refactor: set.lua -> options.lua

--- .config/nvim/lua/zarrios/init.lua
+++ .config/nvim/lua/zarrios/init.lua
@@ -1,10 +1,4 @@
 require("zarrios.remap")
 require("zarrios.lazy")
-require("zarrios.set")
+require("zarrios.options")
 
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
-
--- Make copilot disabled on default
-vim.g.copilot_enabled = false
