69720bca4a25c1a599dc0480a68d6e54eef1f280
iZarrios <zarriose@gmail.com>
2024-04-15 19:43:09 +0200

    fmt

--- .config/nvim/lua/zarrios/lazy.lua
+++ .config/nvim/lua/zarrios/lazy.lua
@@ -15,9 +15,10 @@ vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
     {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-                              -- or, branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        -- or, branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
     },
     {
         'ThePrimeagen/harpoon',
@@ -38,21 +39,21 @@ local plugins = {
         'VonHeikemen/lsp-zero.nvim',
         dependencies = {
             -- LSP Support
-             'neovim/nvim-lspconfig' ,
-             'williamboman/mason.nvim' ,
-             'williamboman/mason-lspconfig.nvim' ,
+            'neovim/nvim-lspconfig',
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
 
             -- Autocompletion
-             'hrsh7th/nvim-cmp' ,
-             'hrsh7th/cmp-buffer' ,
-             'hrsh7th/cmp-path' ,
-             'saadparwaiz1/cmp_luasnip' ,
-             'hrsh7th/cmp-nvim-lsp' ,
-             'hrsh7th/cmp-nvim-lua' ,
+            'hrsh7th/nvim-cmp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lua',
 
             -- Snippets
-             'L3MON4D3/LuaSnip' ,
-             'rafamadriz/friendly-snippets' ,
+            'L3MON4D3/LuaSnip',
+            'rafamadriz/friendly-snippets',
         }
     },
     'nvim-lualine/lualine.nvim',
