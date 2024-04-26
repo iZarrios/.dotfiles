738f66f65737830842f0373065ddab12256428d2
iZarrios <zarriose@gmail.com>
2024-04-26 15:11:59 +0300

    fix: installing golang script

--- setup/go.sh
+++ setup/go.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-GO_VERSION=$(curl -L https://golang.org/VERSION?m=text | head -n 1)
+GO_VERSION=$(curl -L 'go.dev/VERSION?m=text' | head -n 1)
 curl -Lo go.tar.gz "https://go.dev/dl/${GO_VERSION}.linux-amd64.tar.gz"
 tar -xf go.tar.gz
 mv go ~/custom_build/
