d41e4dacbd29923efaf6f4562773285260815c70
iZarrios <zarriose@gmail.com>
2024-04-27 22:55:18 +0300

    bit: comment dbg flags in bash scripts
    to avoid useless debug lines

--- setup/lazygit.sh
+++ setup/lazygit.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-set -eux
+# set -eux
 
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
