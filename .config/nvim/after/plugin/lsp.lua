7592c5eda0c70ec66822aded9375ebda7b240f5a
iZarrios <zarriose@gmail.com>
2024-05-01 15:11:00 +0300

    add: python ruff lsp

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -69,6 +69,18 @@ lsp.configure('lua_ls', {
     },
 })
 
+-- Configure `ruff-lsp`.
+-- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#ruff_lsp
+-- For the default config, along with instructions on how to customize the settings
+require('lspconfig').ruff_lsp.setup {
+  init_options = {
+    settings = {
+      -- Any extra CLI arguments for `ruff` go here.
+      args = {},
+    }
+  }
+}
+
 cmp.setup({
     sources = {
         { name = 'path' },
