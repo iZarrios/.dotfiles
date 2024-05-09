f9c26b15775e2088cce0dc732a9c4290a3fa99dc
iZarrios <zarriose@gmail.com>
2024-05-09 03:47:39 +0300

    update: make find_files show all files

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -9,7 +9,9 @@ local opts = {
 
 
 vim.keymap.set('n', '<C-p>', builtin.git_files, opts)
-vim.keymap.set('n', '<leader>ff', builtin.find_files, opts)
+vim.keymap.set('n', '<leader>ff', function()
+    builtin.find_files({ no_ignore = false, hidden = true })
+end)
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, opts)
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, opts)
 vim.keymap.set('n', '<leader>ps', function()
