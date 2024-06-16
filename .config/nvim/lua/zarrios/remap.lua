3644716ac5f06ead447bcb7ff950eb84c6f00e84
iZarrios <zarriose@gmail.com>
2024-06-16 13:32:49 +0300

    fmt

--- .config/nvim/lua/zarrios/remap.lua
+++ .config/nvim/lua/zarrios/remap.lua
@@ -16,13 +16,11 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-
 vim.keymap.set("n", "gd", "gdzz")
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 vim.keymap.set("n", "<leader>x", "<cmd>:!chmod +x %<CR>")
-
 
 -- go / golang err handling
 vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
