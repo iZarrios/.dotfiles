36c606bec7f40c2ccd29ead184c05a53fe0074b4
iZarrios <zarriose@gmail.com>
2024-07-01 15:54:00 +0300

    add: get-pip.sh

---
+++ setup/get-pip.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+# set -eux
+
+python_cmd=""
+
+ifcommand -v python3 &> /dev/null; then
+  python_cmd="python3"
+elif command -v python &> /dev/null; then
+  python_cmd="python"
+else
+  echo "Python is not found"
+  exit 1
+fi
+
+URL="https://bootstrap.pypa.io/get-pip.py"
+
+curl -L "$URL" | "$python_cmd"
