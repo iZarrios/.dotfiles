5df7a3d5b705ffe01f1913e7112d5a24630aaef0
iZarrios <zarriose@gmail.com>
2024-07-01 20:10:49 +0300

    add: btop.sh

---
+++ setup/btop.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+# set -eux
+
+BTOP_VERSION=$(curl -s "https://api.github.com/repos/aristocratos/btop/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo btop.tbz "https://github.com/aristocratos/btop/releases/download/v${BTOP_VERSION}/btop-x86_64-linux-musl.tbz"
+tar -xf btop.tbz
+sudo cp ./btop/bin/btop /usr/local/bin/
