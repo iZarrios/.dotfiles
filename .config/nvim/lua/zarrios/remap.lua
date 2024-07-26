33f3b50873afd78e0c49510c12bddde19641d744
iZarrios <zarriose@gmail.com>
2024-07-26 13:05:55 +0300

    add: "-" remap to netrw

--- .config/nvim/lua/zarrios/remap.lua
+++ .config/nvim/lua/zarrios/remap.lua
@@ -1,6 +1,7 @@
 local opts = { noremap = true, silent = true }
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, opts)
+vim.keymap.set("n", "-", vim.cmd.Ex, opts)
 
 vim.keymap.set("i", "jj", "<Esc>", opts)
 vim.keymap.set("i", "kj", "<Esc>", opts)
