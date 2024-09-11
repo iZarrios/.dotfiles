80ca5646c4d31a6d0c9ca4319882c972524a8eb5
iZarrios <zarriose@gmail.com>
2024-09-12 02:08:04 +0300

    add: shell setup script

---
+++ setup/shell.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+# set -eux
+
+if command -v curl &> /dev/null; then
+    # installing zsh
+    sudo apt install zsh
+    # making zsh default shell
+    chsh -s `which zsh`
+    # installing zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    ### Installing the plugins
+
+    # zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+    # fast-syntax-highlighting
+    git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
+        ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+fi
+
