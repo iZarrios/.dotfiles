6a059d292460a9bf8161c53a30c1fab1ec330f5c
iZarrios <zarriose@gmail.com>
2024-09-12 23:50:10 +0300

    fix: ripgrep.sh script

--- setup/ripgrep.sh
+++ setup/ripgrep.sh
@@ -5,19 +5,19 @@ if command -v cargo &> /dev/null; then
     # if cargo is found then we can install alacritty
     if command -v rg &> /dev/null; then
         # if ripgrep is found then we need to update it
-        rm -f ~/bin/rg # using -f flag to not produce an error if the file does not exist
+        rm -f ~/.local/bin//rg # using -f flag to not produce an error if the file does not exist
         cd ~/custom_build/ripgrep
         cargo build --release
-        ln -s `realpath ./target/release`/rg ~/bin/rg
+        ln -sf `realpath ./target/release`/rg ~/bin/rg
     else
         mkdir -p ~/custom_build
         cd ~/custom_build
         git clone git@github.com:BurntSushi/ripgrep.git
-        cd alacritty
+        cd ripgrep
         # Installing dependencies
         # https://github.com/alacritty/alacritty/blob/master/INSTALL.md#debianubuntu
         cargo build --release
-        ln -s `realpath ./target/release`/rg ~/bin/rg
+        ln -sf `realpath ./target/release`/rg ~/.local/bin/rg
     fi
 else
     echo "'cargo' has to be installed"
