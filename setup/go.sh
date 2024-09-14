12244e2192c3f5e25aed9a3dc62ca616921e2099
iZarrios <zarriose@gmail.com>
2024-09-14 14:12:10 +0300

    docs: go.sh

--- setup/go.sh
+++ setup/go.sh
@@ -5,7 +5,10 @@ GO_VERSION=$(curl -L 'go.dev/VERSION?m=text' | head -n 1)
 curl -Lo go.tar.gz "https://go.dev/dl/${GO_VERSION}.linux-amd64.tar.gz"
 tar -xf go.tar.gz
 mv go ~/custom_build/
-rm go.tar.gz 
+rm go.tar.gz
+
+# NOTE: now you need to have ~/custom_build/go/bin in your `PATH`
 
 # If you wanna install the LSP
-# go install -v golang.org/x/tools/gopls@latest
+echo "You can install gopls (Go LSP server)"
+echo -e "\e[1mgo install -v golang.org/x/tools/gopls@latest\e[0m"
