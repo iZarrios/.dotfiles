34f8937df02fefc21a5ae401c47a6e63615ee3b8
iZarrios <zarriose@gmail.com>
2024-09-14 14:11:14 +0300

    update: btop dest to .local/bin

--- setup/btop.sh
+++ setup/btop.sh
@@ -4,4 +4,4 @@
 BTOP_VERSION=$(curl -s "https://api.github.com/repos/aristocratos/btop/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo btop.tbz "https://github.com/aristocratos/btop/releases/download/v${BTOP_VERSION}/btop-x86_64-linux-musl.tbz"
 tar -xf btop.tbz
-sudo cp ./btop/bin/btop /usr/local/bin/
+sudo cp ./btop/bin/btop ~/.local/bin
