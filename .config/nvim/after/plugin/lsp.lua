0433cc87d363730f82a7e66d803be0e970da19f9
iZarrios <zarriose@gmail.com>
2024-12-04 21:21:36 +0200

    Update: add clang-tidy in clangd config

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -78,6 +78,7 @@ lsp.configure('lua_ls', {
 
 
 lspcfg.clangd.setup {
+    cmd = { 'clangd', '--background-index', '--clang-tidy' },
     -- custom on attach function for clangd
     on_attach = function(_, bufnr)
         local opts = { noremap = true, silent = true, buffer = bufnr }
