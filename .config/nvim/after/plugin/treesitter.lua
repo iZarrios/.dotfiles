bddc4b9cf355877e3be7f4d4fd01dee2f986d97a
iZarrios <zarriose@gmail.com>
2024-12-06 18:16:45 +0200

    update: nvim-treesitter config

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,4 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
+    ensure_installed = { "c", "lua" },
     -- A list of parser names, or "all"
     -- Install parsers synchronously (only applied to `ensure_installed`)
     -- sync_install = false,
@@ -11,10 +12,18 @@ require 'nvim-treesitter.configs'.setup {
         -- `false` will disable the whole extension
         enable = true,
 
+        disalbe = function(_, buf)
+            local max_file_size = 100 * 1024 -- 100KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_file_size then
+                return true
+            end
+        end,
+
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
+        additional_vim_regex_highlighting = true,
     },
 }
