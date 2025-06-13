91c557534e98ca488d5d5770cc13b40b66ad44c2
iZarrios <esalama057@gmail.com>
2025-06-13 13:47:36 +0300

    update: telescope config

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -9,7 +9,11 @@ local opts = {
 
 vim.keymap.set('n', '<C-p>', builtin.git_files, opts)
 vim.keymap.set('n', '<leader>ff', function()
-  builtin.find_files({ no_ignore = false, hidden = true })
+    -- I do it like this in case there is a specific theme I want to load in the future
+    local tbl = {} -- require('telescope.themes').get_dropdown({})
+    tbl.no_ignore = false
+    tbl.hidden = true
+    builtin.find_files(tbl)
 end)
 
 vim.keymap.set('n', '<leader>fg', function()
@@ -21,11 +25,12 @@ vim.keymap.set('n', '<leader>fg', function()
 end)
 
 vim.keymap.set('n', '<leader>fs', builtin.current_buffer_fuzzy_find, opts)
+vim.keymap.set('n', '<leader>ss', builtin.lsp_document_symbols, opts)
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, opts)
 vim.keymap.set('n', '<leader>ps', function()
-  builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-vim.keymap.set('n', '<leader>sd', builtin.diagnostics, opts)
+vim.keymap.set('n', '<leader>fd', builtin.diagnostics, opts)
 
 -- I never use this
 -- vim.keymap.set('n', '<leader>fb', builtin.buffers, opts)
