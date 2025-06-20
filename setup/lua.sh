112a3d09a5a87e88e1544e6c0bc87c9b676142c9
iZarrios <zarriose@gmail.com>
2025-06-20 05:00:39 +0300

    fix: lua.sh
    removed unecessary lines

--- setup/lua.sh
+++ setup/lua.sh
@@ -19,13 +19,3 @@ make -j`nproc`
 # make symlink to local binaries (which is in my PATH)
 mkdir -p "$HOME/.local/bin"
 ln -sf `realpath lua` "$HOME/.local/bin/"
-
-cd "$HOME/custom_build"
-
-#now we want to install the lua language server
-LUA_LSP_VERSION=$(curl -s "https://api.github.com/repos/LuaLS/lua-language-server/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lua-lsp.tar.gz "https://github.com/LuaLS/lua-language-server/releases/download/${LUA_LSP_VERSION}/lua-language-server-${LUA_LSP_VERSION}-linux-arm64.tar.gz"
-tar xf lua-lsp.tar.gz
-# remove the tar file that we downloaded
-# rm lua-lsp.tar.gz
-
