1e189ca16a5c43fdd3e4456a1f82b3aea1d2e35a
iZarrios <esalama057@gmail.com>
2025-06-26 23:42:51 +0300

    refactor: coloscheme config into its own file

---
+++ .config/nvim/lua/zarrios/colors.lua
@@ -0,0 +1,17 @@
+require("zarrios.dracula")
+
+function ColorMyPencils(color)
+    if color == 'dracula' then
+        local drac = require("zarrios.dracula")
+        drac.Dracula()
+    else
+        vim.cmd.colorscheme(color)
+    end
+
+    -- -- transparent background
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- -- make floating windows transparent
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+ColorMyPencils('dracula')
--- .config/nvim/lua/zarrios/init.lua
+++ .config/nvim/lua/zarrios/init.lua
@@ -1,20 +1,5 @@
 require("zarrios.globals")
 require("zarrios.remap")
 require("zarrios.options")
-require("zarrios.dracula")
+require("zarrios.colors")
 
-function ColorMyPencils(color)
-    if color == 'dracula' then
-        local drac = require("zarrios.dracula")
-        drac.Dracula()
-    else
-        vim.cmd.colorscheme(color)
-    end
-
-    -- -- transparent background
-    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    -- -- make floating windows transparent
-    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
-ColorMyPencils('dracula')
