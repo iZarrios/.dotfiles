a3f3638068bca413e05096795dde854308a9dec6
iZarrios <esalama057@gmail.com>
2025-06-27 14:35:56 +0300

    remove: ts_ls LSP

--- .config/nvim/lsp/ts_ls.lua
+++
@@ -1,7 +0,0 @@
-return {
-    capabilities = vim.tbl_deep_extend(
-        'force',
-        vim.lsp.protocol.make_client_capabilities(),
-        require('blink.cmp').get_lsp_capabilities({}, false)
-    ),
-};
