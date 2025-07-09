e18a64bbe675045477baaf1b6b5fb250dd52628e
iZarrios <esalama057@gmail.com>
2025-07-09 18:50:37 +0300

    bit

--- .config/nvim/lsp/rust_analyzer.lua
+++ .config/nvim/lsp/rust_analyzer.lua
@@ -1,10 +1,10 @@
 return {
     cmd = { "rust-analyzer" },
     filetypes = { "rust" },
+    root_markers = { "Cargo.toml"},
     capabilities = vim.tbl_deep_extend(
         'force',
         vim.lsp.protocol.make_client_capabilities(),
         require('blink.cmp').get_lsp_capabilities({}, false)
     ),
-    root_markers = { "Cargo.toml"},
 };
