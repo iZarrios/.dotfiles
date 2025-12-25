5e6717600c8479f6b7b70120ada4bcb71cbbb97b
iZarrios <esalama057@gmail.com>
2025-12-25 20:10:49 +0200

    fmt: lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,100 +1,101 @@
 local hide_in_width = function()
-    return vim.fn.winwidth(0) > 80
+  return vim.fn.winwidth(0) > 80
 end
 
 local diagnostics = {
-    "diagnostics",
-    sources = { "nvim_diagnostic" },
-    sections = { "error", "warn" },
-    symbols = { error = "✖  ", warn = "⚠  " },
+  "diagnostics",
+  sources = { "nvim_diagnostic" },
+  sections = { "error", "warn" },
+  symbols = { error = "✖  ", warn = "⚠  " },
 
-    colored = false,
-    update_in_insert = false,
-    always_visible = true,
+  colored = false,
+  update_in_insert = false,
+  always_visible = true,
 }
 
 local diff = {
-    "diff",
-    colored = true,
-    symbols = { added = "+", modified = "~", removed = "-" }, -- changes diff symbols
-    cond = hide_in_width
+  "diff",
+  colored = true,
+  symbols = { added = "+", modified = "~", removed = "-" }, -- changes diff symbols
+  cond = hide_in_width
 }
 
 local filename = {
-    "filename",
-    file_status = true,    -- Displays file status (readonly status, modified status)
-    newfile_status = true, -- Display new file status (new file means no write after created)
-    path = 1,              -- 0: Just the filename
-    -- 1: Relative path
-    -- 2: Absolute path
-    -- 3: Absolute path, with tilde as the home directory
-    -- 4: Filename and parent dir, with tilde as the home directory
+  "filename",
+  file_status = true,    -- Displays file status (readonly status, modified status)
+  newfile_status = true, -- Display new file status (new file means no write after created)
+  path = 1,              -- 0: Just the filename
+  -- 1: Relative path
+  -- 2: Absolute path
+  -- 3: Absolute path, with tilde as the home directory
+  -- 4: Filename and parent dir, with tilde as the home directory
 
-    shorting_target = 40,      -- Shortens path to leave 40 spaces in the window
-    symbols = {
-        modified = '[+]',      -- Text to show when the file is modified.
-        readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
-        unnamed = '[No Name]', -- Text to show for unnamed buffers.
-        newfile = '[New]',     -- Text to show for newly created file before first write
-    }
+  shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+  symbols = {
+    modified = '[+]',      -- Text to show when the file is modified.
+    readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+    unnamed = '[No Name]', -- Text to show for unnamed buffers.
+    newfile = '[New]',     -- Text to show for newly created file before first write
+  }
 }
 
 local mode = {
-    "mode",
-    fmt = function(str)
-        return "--" .. str .. "--"
-    end,
-    padding = 1,
+  "mode",
+  fmt = function(str)
+    return "--" .. str .. "--"
+  end,
+  padding = 1,
 }
 
 local filetype = {
-    "filetype",
-    icons_enabled = false,
-    icon = nil,
-    padding = 1,
+  "filetype",
+  icons_enabled = false,
+  icon = nil,
+  padding = 1,
 }
 
 local branch = {
-    "branch",
-    icons_enabled = false,
-    icon = nil,
-    padding = 1,
+  "branch",
+  icons_enabled = false,
+  icon = nil,
+  padding = 1,
 }
 
 local location = {
-    "location",
-    padding = 1,
+  "location",
+  padding = 1,
 }
 
+---@module 'lazy'
+---@type LazySpec
 return {
-    'nvim-lualine/lualine.nvim',
-    config = {
-
-        options = {
-            theme = 'auto',
-            icons_enabled = false,
-            component_separators = { left = "|", right = "|" },
-            section_separators = { left = "|", right = "|" },
-            disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
-            always_divide_middle = true,
-        },
-        sections = {
-            lualine_a = { branch, diagnostics },
-            lualine_b = { mode, filename },
-            lualine_c = {},
-            lualine_x = { "encoding", diff, filetype, location },
-            lualine_y = {},
-            lualine_z = {},
-        },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = { filename },
-            lualine_x = { location },
-            lualine_y = {},
-            lualine_z = {},
-        },
-        tabline = {},
-        extensions = {},
-    }
+  'nvim-lualine/lualine.nvim',
+  opts = {
+    options = {
+      theme = 'auto',
+      icons_enabled = false,
+      component_separators = { left = "|", right = "|" },
+      section_separators = { left = "|", right = "|" },
+      disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+      always_divide_middle = true,
+    },
+    sections = {
+      lualine_a = { branch, diagnostics },
+      lualine_b = { mode, filename },
+      lualine_c = {},
+      lualine_x = { "encoding", diff, filetype, location },
+      lualine_y = {},
+      lualine_z = {},
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { filename },
+      lualine_x = { location },
+      lualine_y = {},
+      lualine_z = {},
+    },
+    tabline = {},
+    extensions = {},
+  }
 };
