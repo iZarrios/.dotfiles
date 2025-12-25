68fa793d2176af315de462985ea0dea2ebada2d9
iZarrios <esalama057@gmail.com>
2025-12-25 22:42:34 +0200

    add: neogit

---
+++ .config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,16 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+  "NeogitOrg/neogit",
+  lazy = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "sindrets/diffview.nvim",        -- optional - Diff integration
+    "nvim-telescope/telescope.nvim", -- optional
+  },
+  cmd = "Neogit",
+  keys = {
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+  },
+
+};
