80fe3e48e50a4ed71f6b61d7698003d4544da0ec
iZarrios <esalama057@gmail.com>
2025-12-25 19:38:31 +0200

    update: relative path for file path in lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -24,7 +24,7 @@ local filename = {
     "filename",
     file_status = true,    -- Displays file status (readonly status, modified status)
     newfile_status = true, -- Display new file status (new file means no write after created)
-    path = 4,              -- 0: Just the filename
+    path = 1,              -- 0: Just the filename
     -- 1: Relative path
     -- 2: Absolute path
     -- 3: Absolute path, with tilde as the home directory
