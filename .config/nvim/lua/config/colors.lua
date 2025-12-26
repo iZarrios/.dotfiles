76ab8e1a438d5e2e4b4a6bc72979154356a4759c
iZarrios <esalama057@gmail.com>
2025-12-26 15:09:18 +0200

    remove: custom dracula theme

--- .config/nvim/lua/config/colors.lua
+++ .config/nvim/lua/config/colors.lua
@@ -1,12 +1,5 @@
-require("config.dracula")
-
 function ColorMyPencils(color)
-    if color == 'dracula' then
-        local drac = require("config.dracula")
-        drac.Dracula()
-    else
-        vim.cmd.colorscheme(color)
-    end
+    vim.cmd.colorscheme(color)
 
     -- -- transparent background
     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
