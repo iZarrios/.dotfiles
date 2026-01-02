613791a6944edeff60e435ed4b42fd673523e290
iZarrios <esalama057@gmail.com>
2026-01-02 23:28:16 +0200

    fmt: zsh script setup

--- setup/zsh.sh
+++ setup/zsh.sh
@@ -11,7 +11,8 @@ if command -v zsh &> /dev/null; then
     ### Installing the plugins
 
     # zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions \
+        ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
     # fast-syntax-highlighting
     git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
