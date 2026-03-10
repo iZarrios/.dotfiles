35a8a821e86e8d0c9bc916ca4ff3ea7411ce145f
iZarrios <esalama057@gmail.com>
2026-03-10 03:08:30 +0200

    update: nvim colorscheme default on error

--- .config/nvim/lua/config/colors.lua
+++ .config/nvim/lua/config/colors.lua
@@ -1,10 +1,17 @@
 function ColorMyPencils(color)
-    vim.cmd.colorscheme(color)
+    local success, err = pcall(function()
+        vim.cmd.colorscheme(color)
+    end)
 
-    -- -- transparent background
+    if not success then
+        print(string.format("Color scheme '%s' not found: %s", color, err))
+        print("Falling back to default")
+        vim.cmd.colorscheme('default')
+    end
+
+    -- transparent background
     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    -- -- make floating windows transparent
     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-ColorMyPencils('terafox')
+ColorMyPencils('gruvbox')
