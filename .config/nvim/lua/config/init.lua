5bb3c70d73d4e45169327948b13a60788cdade9f
iZarrios <esalama057@proton.me>
2026-03-30 15:40:40 +0200

    fix: lsp requiring blink.cmp before it was loaded

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -1,11 +1,11 @@
 -- NOTE: we need to require "config.options" before Lazy
 
-require("config.lsp")
 require("config.options")
 require("config.globals")
 require("config.custom_cmds")
 require("config.keymaps")
 
 require("config.lazy")
+require("config.lsp")
 require("config.colors")
 
