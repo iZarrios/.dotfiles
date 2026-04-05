1043e64de70cc4633faa12fa1f76f5ded49777ec
iZarrios <esalama057@proton.me>
2026-04-05 10:08:39 +0200

    update: 0.12 lsp.lua

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -1,14 +1,39 @@
 vim.lsp.enable({
-    "gopls",
+    -- "gopls",
     "lua_ls",
     "clangd",
     -- "pyright",
     "ruff",
     "ty",
     "rust_analyzer",
-    "ts_ls",
+    -- "ts_ls",
 })
 
+
+-- commenting this as when using rust it clutters the messages box
+-- -- vim.api.nvim_create_autocmd('LspProgress', {
+-- --   callback = function(ev)
+-- --     local client = vim.lsp.get_client_by_id(ev.data.client_id)
+-- --     local value = ev.data.params.value
+-- --
+-- --     if not client or not value then return end
+-- --
+-- --     -- CUSTOMIZE YOUR MESSAGE HERE
+-- --     local icon = value.kind == 'end' and '✅' or '⏳'
+-- --     local client_name = client.name
+-- --     local msg = value.message or (value.kind == 'end' and 'Finished' or 'In progress...')
+-- --     local display_text = string.format("%s [%s] %s", icon, client_name, msg)
+-- --
+-- --     vim.api.nvim_echo({ { display_text } }, false, {
+-- --       id = 'lsp.' .. ev.data.client_id,
+-- --       kind = 'progress',
+-- --       source = 'vim.lsp',
+-- --       title = value.title,
+-- --       status = value.kind ~= 'end' and 'running' or 'success',
+-- --       percent = value.percentage,
+-- --     })
+-- --   end,
+-- -- })
 
 vim.diagnostic.config({
     underline = true,
