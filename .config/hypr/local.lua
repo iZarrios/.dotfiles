b1001cf9d950e4b76c1ad5bce7edcbe16d411d35
iZarrios <esalama057@proton.me>
2026-05-16 13:08:29 -0400

    add: getHostName utitlity; update: hyprland monitor setup

--- .config/hypr/local.lua
+++ .config/hypr/local.lua
@@ -1,6 +1,26 @@
+
+function getHostname()
+	local f = io.popen("/bin/hostnamectl hostname")
+	local hostname = f:read("*a") or ""
+	f:close()
+	hostname = string.gsub(hostname, "\n$", "")
+	return hostname
+end
+
+if getHostname() == "biluka" then
 hl.monitor({
     output   = "DP-1",
-    mode     = "1920x1080@143.98",
+    mode     = "1920x1080@144",
     position = "0x0",
     scale    = 1.0,
 })
+else
+hl.monitor({
+    output   = "eDP-1",
+    mode     = "1920x1080@60",
+    position = "0x0",
+    scale    = 1.0,
+})
+end
+
+
