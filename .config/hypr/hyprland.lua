d22d1ad8092e1d0654bcbabecc01843f81971f81
iZarrios <esalama057@proton.me>
2026-07-05 21:23:36 -0400

    update: display setup hyprland

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -46,7 +46,7 @@ end
 apply_invariant({
   biluka = function()
     hl.monitor({
-      output   = "HDMI-A-1",
+      output   = "DP-1",
       mode     = "1920x1080@144.00",
       position = "0x0",
       scale    = 1.0,
