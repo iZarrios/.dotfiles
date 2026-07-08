a9934e47549eba16a5531c4e239a770f401b527c
iZarrios <esalama057@proton.me>
2026-07-08 09:18:59 -0400

    update: neovim neogit config

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -8,9 +8,45 @@ return {
     "sindrets/diffview.nvim",        -- optional - Diff integration
     "nvim-telescope/telescope.nvim", -- optional
   },
+config = function()
+  require("neogit").setup({
+    kind = "split",
+    -- Add this block right here:
+    filewatcher = {
+      enabled = true,
+    },
+    graph_style = "unicode",
+    -- This is the setting you are looking for!
+    side_by_side = true,
+
+    integrations = {
+      diffview = true
+    },
+    commit_editor = {
+      kind = "split",
+      show_staged_diff = true,
+      staged_diff_split_kind = "split"
+    }
+  })
+end,
+
+  -- config = function()
+  --   require("neogit").setup({
+  --     kind = "split",   -- Opens the main Neogit buffer in a split
+  --     integrations = {
+  --       diffview = true -- Enables integration with diffview.nvim
+  --     },
+  --     commit_editor = {
+  --       kind = "split",                  -- Shows the staged diff below the commit message
+  --       show_staged_diff = true,
+  --       staged_diff_split_kind = "split" -- Can be "split", "vsplit", or "auto"
+  --     }
+  --   })
+  -- end,
+
   cmd = "Neogit",
   keys = {
-    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+    { "<leader>gg", function() require("neogit").open({ kind = "replace" }) end, desc = "Show Neogit UI Split" }
   },
 
 };
